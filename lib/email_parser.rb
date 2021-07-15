df62528ba51f2ae2fe9d08d2198059276eaa5a6f
Megan McCarty <megan_mccarty@hotmail.com>
2021-07-15 10:50:08 -0400

    commit tree cbfa1b24c2a0f53464ee99c6de405e3513bd99fb

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -2,3 +2,24 @@
 # emails. The parse method on the class should separate them into
 # unique email addresses. The delimiters to support are commas (',')
 # or whitespace (' ').
+
+class EmailAddressParser
+
+    attr_accessor :email_addresses
+
+    def initialize(email_addresses)
+        @email_addresses = email_addresses
+    end
+
+    def parse
+        # take in the email_addresses string and split it by either space or comma
+        new_email_array = @email_addresses.split(/, |\s/)
+        new_email_array.uniq
+    end
+end
+
+megan = EmailAddressParser.new("megan_mccarty@hotmail.com lepgirl77@yahoo.com")
+stranger = EmailAddressParser.new("stranger@gmail.com, strange77@yahoo.com")
+
+megan.parse
+stranger.parse
